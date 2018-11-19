a9449d2566cb7156771ff8d884becd0d06ea780e
Matteo Bilotta <mbilotta@linkgroup.it>
2018-11-19 18:33:37 +0100

    [WIP] Added Bootstrap v. 4.1.3 .

---
+++ src/requirements.js
@@ -0,0 +1,2 @@
+import "bootstrap";
+import "./requirements.scss";
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/requirements.js",
+  output: {
+    filename: "dependencies.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: function () {
+                return [ require("autoprefixer") ];
+              }
+            }
+          },
+          { loader: "sass-loader" }
+        ]
+      }
+    ]
+  }
+};
